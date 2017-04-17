abf8d8a1b7a187dceb7d4ab727561902a30625a3
Cristian Perdomo <cristianeprdomo@unicauca.edu.co>
2017-04-17 13:03:03 -0500

    Todos archivos funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 import { PizzaData } from '../providers/pizzas-data';
 import { BebidaData } from '../providers/bebidas-data';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { BebidaPage } from "../pages/bebida/bebida";
-import { PizzaPage } from "../pages/pizza/pizza";
 
 @NgModule({
   declarations: [
@@ -18,9 +16,7 @@ import { PizzaPage } from "../pages/pizza/pizza";
     AboutPage,
     BebidaPage,
     HomePage,
-    TabsPage,
     LoginPage,
-    PizzaPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -31,9 +27,7 @@ import { PizzaPage } from "../pages/pizza/pizza";
     AboutPage,
     BebidaPage,
     HomePage,
-    TabsPage,
     LoginPage,
-    PizzaPage
   ],
   providers: [
     StatusBar,
